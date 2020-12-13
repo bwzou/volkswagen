05b121571782c3e369b1d6ca26b15959f6166802
bwzou@ust.hk <bwzou@ust.hk>
2020-12-13 16:20:25 +0800

    first commit

---
+++ src/views/AvoidFakes.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: "AvoidFakes"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Comparison.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+  export default {
+    name: "Comparison"
+  }
+</script>
+
+<style scoped>
+
+</style>
